4df3a86db6fcdd29d6ccdc420da5ce5efe542222
GuiHSM <Guihenrisampaio@gmail.com>
2023-02-05 11:12:57 -0300

    criarCardMoves criado

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var alterarSrc = (id, nome)=>{
+var alterarSrcPokemon = (id, nome)=>{
     let request = new XMLHttpRequest()
 
     request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+nome, true)
@@ -7,16 +7,58 @@ var alterarSrc = (id, nome)=>{
     request.onload = function () {
         let data = JSON.parse(this.response)
 
-    if (request.status >= 200 && request.status < 400) {
-        let imagem = document.getElementById(id);
-        imagem.src = data.sprites.front_default;
-    } else {
-        console.log('error')
-    }
+        if (request.status >= 200 && request.status < 400) {
+            let imagem = document.getElementById(id);
+            imagem.src = data.sprites.front_default;
+        } else {
+            console.log('error')
+        }
     }
     request.send()
 }
 
-alterarSrc("imagem","pikachu");
+var criarCardMoves = (id,nome)=>{
+    let request = new XMLHttpRequest()
 
+    request.open('GET', `https://pokeapi.co/api/v2/pokemon/${nome}`, true)
+
+    request.onload = function () {
+        let data = JSON.parse(this.response)
+
+        if (request.status >= 200 && request.status < 400) {
+            let div = document.getElementById(id);
+            let html = `<img src="${data.sprites.front_default}">`;
+            /*for(let i=0;i<data.moves;i++){
+                let move = data.moves[i];
+            }*/
+            html+=`<div id=${nome}>`
+            data.moves.forEach(move=>{
+                let auxiliar="";
+                for(let i=0;i<move.version_group_details.length;i++){
+                    let version=move.version_group_details[i]
+                    if(version.version_group.name=="firered-leafgreen"){
+                        auxiliar=`<p>Método: ${version.move_learn_method.name}`
+                        if(version.move_learn_method.name=="level-up"){
+                            auxiliar+=` Nível: ${version.level_learned_at}`
+                        }
+                        auxiliar+="<p>"
+                        break;
+                    }
+                }
+                if(auxiliar!=""){
+                    html+=`<p>Nome: ${move.move.name}</p>`
+                    html+=auxiliar;
+                }
+            })
+            html+=`</div>`
+            div.innerHTML=html;
+        } else {
+            console.log('error')
+        }
+    }
+    request.send()
+}
+
+
+criarCardMoves("teste","pikachu");
 
